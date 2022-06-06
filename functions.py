5b487b025a158bf5834e35bcbc2c68b59212e453
Josip Keresman <josipkeresman@gmail.com>
2022-06-06 18:30:35 +0200

    register && login , v1

--- functions.py
+++ functions.py
@@ -1,9 +1,9 @@
 from user import User, db
 
 
-def check_if_user_exists(user_email: str) -> User:
+def check_if_user_exists(session_token: str) -> User:
     user = None
-    if user_email:
-        user = db.query(User).filter_by(email=user_email).first()
+    if session_token:
+        user = db.query(User).filter_by(session_token=session_token).first()
 
     return user
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, url_for, request, redirect, make_response
+from flask import Flask, render_template, request, make_response, redirect, url_for
 from user import User, db
 from functions import check_if_user_exists
 import constants
 import random
+import hashlib
+import uuid
 
 app = Flask(__name__)
 db.create_all()
@@ -11,31 +13,79 @@ db.create_all()
 @app.route("/", methods=["GET"])
 def index():
 
-    user_email = request.cookies.get("email")
+    session_token = request.cookies.get("session_token")
 
-    user = check_if_user_exists(user_email)
+    user = check_if_user_exists(session_token=session_token)
+    if user is not None:
+        return render_template("game.html")
 
     response = make_response(render_template("index.html", user=user))
     return response
 
 
-@app.route("/login", methods=["POST"])
+@app.route("/register", methods=["POST", "GET"])
+def register():
+
+    if request.method == "POST":
+
+        first_name = request.form.get("first-name")
+        last_name = request.form.get("last-name")
+        username = request.form.get("username")
+        password = request.form.get("password")
+        hashed_password = hashlib.sha256(password.encode()).hexdigest()
+        email = request.form.get("email")
+        session_token = str(uuid.uuid4())
+
+        secret_number = random.randint(constants.RANDOM_LOW_LIMIT, constants.RANDOM_HIGH_LIMIT)
+
+        user = User(first_name=first_name,
+                    last_name=last_name,
+                    email=email,
+                    secret_number=secret_number,
+                    password=hashed_password,
+                    username=username,
+                    session_token=session_token,
+                    )
+
+        user.save()
+
+        response = make_response(render_template("login.html"))
+
+    else:
+        response = make_response(render_template("register.html"))
+
+    return response
+
+
+@app.route("/login", methods=["POST", "GET"])
 def login():
 
-    first_name = request.form.get("first-name")
-    last_name = request.form.get("last-name")
-    email = request.form.get("email")
-    secret_number = random.randint(constants.RANDOM_LOW_LIMIT, constants.RANDOM_HIGH_LIMIT)
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
 
-    user = db.query(User).filter_by(email=email).first()
+        user = db.query(User).filter_by(username=username).first()
+        hashed_password = hashlib.sha256(password.encode()).hexdigest()
 
-    if not user:
-        user = User(first_name=first_name, last_name=last_name, email=email, secret_number=secret_number)
+        if user.password == hashed_password:
+            response = make_response(render_template("game.html"))
+            response.set_cookie("session_token", user.session_token, httponly=True, samesite="Strict")
+        else:
+            response = make_response(render_template("login.html"))
+    else:
+        response = make_response(render_template("login.html"))
 
-    user.save()
+    return response
+
+
+@app.route("/log-out", methods=["POST"])
+def log_out():
 
     response = make_response(redirect(url_for("index")))
-    response.set_cookie("email", user.email)
+
+    session_token = request.cookies.get("session_token")
+    if session_token:
+        response.set_cookie("session_token", expires=0)
 
     return response
 
@@ -45,8 +95,8 @@ def secret_number_handler():
 
     guess_is_correct = False
 
-    user_email = request.cookies.get("email")
-    user = check_if_user_exists(user_email)
+    session_token = request.cookies.get("session_token")
+    user = check_if_user_exists(session_token=session_token)
 
     user_guess = int(request.form.get("user-guess"))
 
@@ -65,4 +115,4 @@ def secret_number_handler():
 
 
 if __name__ == "__main__":
-    app.run(use_reloader=True)
+    app.run(port=5005, use_reloader=True)
