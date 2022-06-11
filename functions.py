7a69ead3293a44e1345dacc14304d63f751f4948
Josip Keresman <josipkeresman@gmail.com>
2022-06-11 14:17:26 +0200

    change password option

--- functions.py
+++ functions.py
@@ -1,3 +1,6 @@
+import hashlib
+
+import constants
 from user import User, db
 
 
@@ -7,3 +10,16 @@ def get_user_by_session_token(session_token: str) -> User:
         user = db.query(User).filter_by(session_token=session_token).first()
 
     return user
+
+
+def too_short_password_check(password: str) -> bool:
+    return len(password) < constants.PASSWORD_MIN_LENGTH
+
+
+def re_entered_password_check(password: str, re_entered_password: str) -> bool:
+    return password == re_entered_password
+
+
+def correct_password_check(password: str, user: User) -> bool:
+    hashed_password = hashlib.sha256(password.encode()).hexdigest()
+    return hashed_password == user.password
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, make_response, redirect, url_for
-from user import User, db
-from functions import get_user_by_session_token
+from functions import *
 import constants
 import random
 import hashlib
@@ -17,7 +16,7 @@ def index():
 
     user = get_user_by_session_token(session_token=session_token)
 
-    if user is not None:
+    if user:
         return render_template("game.html", user=user)
 
     return render_template("index.html")
@@ -42,28 +41,31 @@ def register():
             user_already_exists = True
             response = make_response(render_template("register.html", user_already_exists=user_already_exists))
 
-        elif re_entered_password != password:
-            incorrect_password = True
-            response = make_response(render_template("register.html", incorrect_password=incorrect_password))
-
         else:
-            hashed_password = hashlib.sha256(password.encode()).hexdigest()
-            session_token = str(uuid.uuid4())
+            if too_short_password_check(password=password):
+                response = make_response(render_template("register.html", too_short_password=True))
 
-            secret_number = random.randint(constants.RANDOM_LOW_LIMIT, constants.RANDOM_HIGH_LIMIT)
+            elif not re_entered_password_check(password=password, re_entered_password=re_entered_password):
+                response = make_response(render_template("register.html", incorrect_password=True))
 
-            user = User(first_name=first_name,
-                        last_name=last_name,
-                        email=email,
-                        secret_number=secret_number,
-                        password=hashed_password,
-                        username=username,
-                        session_token=session_token,
-                        )
+            else:
+                hashed_password = hashlib.sha256(password.encode()).hexdigest()
+                session_token = str(uuid.uuid4())
 
-            user.save()
+                secret_number = random.randint(constants.RANDOM_LOW_LIMIT, constants.RANDOM_HIGH_LIMIT)
 
-            response = make_response(render_template("login.html"))
+                user = User(first_name=first_name,
+                            last_name=last_name,
+                            email=email,
+                            secret_number=secret_number,
+                            password=hashed_password,
+                            username=username,
+                            session_token=session_token,
+                            )
+
+                user.save()
+
+                response = make_response(render_template("login.html"))
 
     return response
 
@@ -84,15 +86,13 @@ def login():
             response = make_response(render_template("login.html", incorrect_username=incorrect_username))
 
         else:
-            hashed_password = hashlib.sha256(password.encode()).hexdigest()
 
-            if user.password == hashed_password:
+            if correct_password_check(password=password, user=user):
                 response = make_response(render_template("game.html", user=user))
                 response.set_cookie("session_token", user.session_token, httponly=True, samesite="Strict")
 
-            elif user.password != hashed_password:
-                incorrect_password = True
-                response = make_response(render_template("login.html", incorrect_password=incorrect_password))
+            else:
+                response = make_response(render_template("login.html", incorrect_password=True))
 
     return response
 
@@ -109,39 +109,76 @@ def logout():
     return response
 
 
-@app.route("/secret-number", methods=["POST"])
+@app.route("/secret-number", methods=["GET", "POST"])
 def secret_number_handler():
 
-    guess_is_correct = False
+    user_guess = request.form.get("user-guess")
 
     session_token = request.cookies.get("session_token")
-
     user = get_user_by_session_token(session_token=session_token)
 
-    user_guess = int(request.form.get("user-guess"))
+    try:
+        int_user_guess = int(user_guess)
+    except ValueError:
+        return render_template("game.html", incorrect_input=True, user_guess=user_guess)
 
-    if user.secret_number == user_guess:
-        guess_is_correct = True
+    if user.secret_number == int_user_guess:
         new_secret_number = random.randint(constants.RANDOM_LOW_LIMIT, constants.RANDOM_HIGH_LIMIT)
         user.secret_number = new_secret_number
         user.save()
 
-    response = make_response(render_template("guess.html",
-                                             guess_is_correct=guess_is_correct,
-                                             secret_number=user.secret_number,
-                                             guess=user_guess,
-                                             user=user,
-                                             ))
+        response = make_response(render_template("guess.html",
+                                                 guess_is_correct=True,
+                                                 guess=int_user_guess,
+                                                 user=user,
+                                                 ))
+    else:
+        response = make_response(render_template("guess.html", guess=int_user_guess, user=user))
+
     return response
 
 
-@app.route("/profile")
+@app.route("/profile", methods=["GET"])
 def profile_handler():
 
     session_token = request.cookies.get("session_token")
     user = get_user_by_session_token(session_token=session_token)
 
     return render_template("profile.html", user=user)
+
+
+@app.route("/change-password", methods=["GET", "POST"])
+def change_password():
+
+    session_token = request.cookies.get("session_token")
+    user = get_user_by_session_token(session_token=session_token)
+
+    response = make_response(render_template("change_password.html", user=user))
+
+    if request.method == "POST":
+        if user:
+            current_password = request.form.get("current-password")
+            new_password = request.form.get("new-password")
+            re_entered_password = request.form.get("password-check")
+
+            if not correct_password_check(password=current_password, user=user):
+                return render_template("change_password.html", incorrect_password=True)
+
+            elif too_short_password_check(password=new_password):
+                return render_template("change_password.html", too_short_password=True)
+
+            elif not re_entered_password_check(password=new_password, re_entered_password=re_entered_password):
+                return render_template("change_password.html", incorrect_re_entered_password=True)
+
+            else:
+                hashed_password = hashlib.sha256(new_password.encode()).hexdigest()
+                user.password = hashed_password
+                user.save()
+
+                response = make_response(render_template("index.html", password_change=True))
+                response.set_cookie("session_token", expires=0)
+
+    return response
 
 
 if __name__ == "__main__":
